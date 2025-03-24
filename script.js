d97e3a2e36f092365f2654ec34a9a9747f660918
haxenfan <haxen.obj.int@gmail.com>
2025-03-24 21:54:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+// script.js
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("/nav.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("nav-container").innerHTML = html;
+    });
+});
